c8bab18ba3c92f70a0b77c5e188f98a5c9d4dbaa
Sojin Lee <sojinlee@Sojinui-MacBookAir.local>
2023-10-16 23:47:09 +0900

    Added the 'AdvertWidget' and the 'ProfilePage'

--- client/src/components/Friend.jsx
+++ client/src/components/Friend.jsx
@@ -7,6 +7,8 @@ import UserImage from "./UserImage";
 import { useNavigate } from "react-router-dom";
 
 const Friend = ({ friendId, name, subtitle, userPicturePath }) => {
+  console.log(`TEST Friend.jsx - name & subtitle: ${name} & ${subtitle}`);
+
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const { _id } = useSelector((state) => state.user);
@@ -42,31 +44,30 @@ const Friend = ({ friendId, name, subtitle, userPicturePath }) => {
   return (
     <FlexBetween>
       <FlexBetween gap="1rem">
-        <UserImage image={userPicturePath} size="55px">
-          <Box
-            onClick={() => {
-              navigate(`/profile/${friendId}`);
-              navigate(0); // TODO: temp solution to refresh components
+        <UserImage image={userPicturePath} size="55px" />
+        <Box
+          onClick={() => {
+            navigate(`/profile/${friendId}`);
+            navigate(0); // TODO: temp solution to refresh components
+          }}
+        >
+          <Typography
+            color={main}
+            variant="h5"
+            fontWeight="500"
+            sx={{
+              "&:hover": {
+                color: palette.primary.light,
+                cursor: "pointer",
+              },
             }}
           >
-            <Typography
-              color={main}
-              variant="h5"
-              fontWeight="500"
-              sx={{
-                "&: hover": {
-                  color: palette.primary.light,
-                  cursor: "pointer",
-                },
-              }}
-            >
-              {name}
-            </Typography>
-            <Typography color={medium} fontSize="0.75rem">
-              {subtitle}
-            </Typography>
-          </Box>
-        </UserImage>
+            {name}
+          </Typography>
+          <Typography color={medium} fontSize="0.75rem">
+            {subtitle}
+          </Typography>
+        </Box>
       </FlexBetween>
       <IconButton
         onClick={() => patchFriend()}
